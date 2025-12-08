1b7e9722dadd33bb44d48c49867c2869bb8286f3
Nathan <69707804+OneShot666@users.noreply.github.com>
2025-12-08 10:58:58 +0100

    Add kitchen assets

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+// Personal TO DO List
+// . Add items
+// . Add player + movement with ZQSD
+// . Add kitchen
+// . Add Uxui (whip)
+// . Make camera follow player
+
+// Official TO DO List
+// ! Non-diegetic interface to manage player’s hands : can carry ingredients and objects 
+// ! Diegetic cooking interface : player can place ingredients in a pot and cook (with in-game adjustable gauge)
+// ! In-game interface allowing player to turn pages of a recipe book
+// ! An inventory menu displaying all available objects along (show descriptions & thumbnail) when player selects a drawer
+// ! -> Same for cupboards or fridge, with tabs allowing player to choose which shelf to display
+// ! Objects have tag to filter inventory display
+// ! Magnifying function to preview contents of cupboard (first available items) without opening full menu
+// L Zoomable inventory menu
+// L Second, non-blocking display mode for the inventory, accessible by clicking the minimize icon
+public class UIManager : MonoBehaviour {
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
